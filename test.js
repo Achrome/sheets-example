03450e11a56a3b4f5d14d7f5dfe29f4b073eb280
Ashwin Mukhija <achromatic.rainbow@gmail.com>
2015-07-26 13:39:21 +0530

    Added some error handling

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 import GAuth from './lib/google-auth';
 const co = require('co');
+const request = require('co-request');
 
-let a = new GAuth('config/creds.json', ['drive']);
 co(function* () {
+  let a = new GAuth('config/creds.json', ['drive.readonly']);
   const token = yield a.getToken();
-  console.log(token);
-});
+  const req = {
+    url: 'https://www.googleapis.com/drive/v2/files/?maxResults=5',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  };
+  const resp = yield request(req);
+  console.log(JSON.parse(resp.body));
+}).catch(console.log);
